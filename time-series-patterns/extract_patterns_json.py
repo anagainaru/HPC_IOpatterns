85022efb684ced772146cfe12d1944d2de9f3125
anagainaru <ana.gainaru@gmail.com>
2022-07-08 13:06:50 -0400

    Extract clusters from time series events

--- time-series-patterns/extract_patterns_json.py
+++ time-series-patterns/extract_patterns_json.py
@@ -7,7 +7,6 @@ import sys
 # Usage: 
 #    python extract_patterns_json.py json_file
 
-
 # Read the json file and keep events timestamp and type
 def read_json_data(file_name):
     inf = open(file_name, "r")
@@ -24,28 +23,96 @@ def read_json_data(file_name):
     min_ts = min([min(events_time[rank]) for rank in events_time])
     max_ts = max([max(events_time[rank]) for rank in events_time])
     print("Execution time %d seconds" %((max_ts - min_ts) / 1000000))
-    #events_delay = {rank: [(events_time[rank][i] - events_time[rank][i-1])/1000000
-    #                       for i in range(1,len(events_time[rank]))]
-    #                for rank in events_time}
-    return [(i - min_ts) / 1000000 for i in events_time], events_type
+    return {rank: [(i - min_ts) / 1000000 for i in events_time[rank]]
+            for rank in events_time}, events_type
+
+# update labels based on the new cluster split
+def create_labels(labels, split_index):
+    new_label = max(labels) + 1
+    current_label = labels[split_index]
+    for i in range(split_index+1, len(labels)):
+        if labels[i] != current_label:
+            break
+        labels[i] = new_label
+    return labels
+
+# Criterias to terminate the clustering before reaching individual
+# events. Currently, the max delay between events needs to be over 3 seconds
+def cannot_split_clusters(labels, max_delay):
+    if max_delay > 3:
+        return False
+    return True
 
 # Hierarchical clustering for the events series 
-def find_clusters():
-    # The silhouette_score gives the average value for all the samples.
-    # This gives a perspective into the density and separation of the formed
-    # clusters
-    silhouette_avg = silhouette_score(events_time, cluster_labels)
-    
+def find_clusters(events_time):
+    events_delay = [(events_time[i] - events_time[i-1])
+                    for i in range(1,len(events_time))]
+    labels = [0] * len(events_time)
+    best_clusters = [0, labels]
+    while len(events_delay) > 0:
+        # Split clusters on the largerst delay between events
+        max_delay = max([events_delay[i] for i in range(len(events_delay))
+                         if labels[i]==labels[i+1]])
+        split_index = [i for i in range(len(events_delay))
+                       if events_delay[i]==max_delay][0]
+        print("Split between timestamp", events_time[split_index],
+              events_time[split_index+1])
+        # if the clusters start computing the silhouette, update the value for
+        # the current selection
+        if max_delay <= 60 and best_clusters[0]==0:
+            silhouette_avg = silhouette_score(events_time.reshape(-1, 1), labels)
+            best_clusters = [silhouette_avg, labels[:]]
+            print("Store current cluster with a score of", silhouette_avg)
+        labels = create_labels(labels, split_index)
+
+        # Compute the silhouette score which gives a score for the density and
+        # separation of the formed clusters (only if the delay between events
+        # is less than a minute, otherwise save the clusters)
+        silhouette_avg = 0
+        if max_delay <= 60:
+            silhouette_avg = silhouette_score(events_time.reshape(-1, 1), labels)
+        print("Clustering score", silhouette_avg)
+        if max_delay <= 60 and silhouette_avg > best_clusters[0]:
+            print("BEST cluster", best_clusters[0], silhouette_avg)
+            best_clusters = [silhouette_avg, labels[:]]
+        if cannot_split_clusters(labels, max_delay):
+            break
+    if best_clusters[0] == 0:
+            best_clusters = [silhouette_avg, labels[:]]
+    print("Total clusters", max(best_clusters[1]))
+    return best_clusters[1]
+
+# Extract all the event patterns describing patterns and their frequency
+def find_patterns(labels, events_type, events_time):
+    patterns = {}
+    cluster_edges = [0] + [i for i in range(len(labels) - 1) if labels[i] != labels[i+1]]
+    print({cluster_edges[i] : events_time[cluster_edges[i]:cluster_edges[i+1]] for i in range(1, len(cluster_edges))})
+    for i in range(1, len(cluster_edges)):
+
+
+# Extract the start clusters based on patterns across all the ranks
+def extract_start_clusters(labels):
+    return 0
+
+# Extract the end clusters based on patterns across all the ranks
+def extract_end_clusters(labels):
+    return 0
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Usage: python %s log_file" %(sys.argv[0]))
         exit()
     
+    labels = {}
     # read the json TAU file
     events_time, events_type = read_json_data(sys.argv[1])
     for rank in events_time:
-        find_clusters(events_time[rank])
+        print("Extract clusters for rank", rank)
+        labels[rank] = find_clusters(np.array(events_time[rank]))
+        find_patterns(labels[rank], events_type[rank], events_time[rank])
+        break
+    extract_start_clusters(labels)
+    extract_end_clusters(labels)
+
     rank = 1
-    print([i for i in events_delay[rank] if i>10])
     print(set(events_type[rank]))
