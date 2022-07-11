3762411fea77c4f90ea2508e892ebbba1f0959c5
anagainaru <ana.gainaru@gmail.com>
2022-07-11 15:57:47 -0400

    Create a new log with extended TAU entries

--- time-series-patterns/extract_patterns_json.py
+++ time-series-patterns/extract_patterns_json.py
@@ -1,11 +1,57 @@
+from difflib import SequenceMatcher
+from itertools import cycle
+from itertools import islice
 import json
 import numpy as np
+from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 import sys
 
 # Parse the JSON log and extract patterns
 # Usage: 
 #    python extract_patterns_json.py json_file
+
+
+def string_distance(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+def roundrobin(iterables):
+    """Yields an item from each iterable, alternating between them.
+
+        >>> list(roundrobin(['ABC', 'D', 'EF']))
+        ['A', 'D', 'E', 'B', 'F', 'C']
+
+    This function produces the same output as :func:`interleave_longest`, but
+    may perform better for some inputs (in particular when the number of
+    iterables is small).
+
+    """
+    # Recipe credited to George Sakkis
+    pending = len(iterables)
+    nexts = cycle(iter(it).__next__ for it in iterables)
+    while pending:
+        try:
+            for next in nexts:
+                yield next()
+        except StopIteration:
+            pending -= 1
+            nexts = cycle(islice(nexts, pending))
+
+# Populate the log with a start and end flag to force a min of two clusters
+def add_edges(events_time, events_type):
+    min_ts = min([min(events_time[rank]) for rank in events_time])
+    max_ts = max([max(events_time[rank]) for rank in events_time])
+    for rank in events_time:
+        if max_ts - events_time[rank][0] < events_time[rank][0] - min_ts:
+            # Populate with a START flag at the beginning of the execution
+            events_time[rank].insert(0, min_ts)
+            events_type[rank].insert(0, ".DEBUGSTART")
+        if max_ts - events_time[rank][-1] > events_time[rank][-1] - min_ts:
+            # Populate with an END flag at the end of the execution
+            events_time[rank].append(max_ts)
+            events_type[rank].append(".DEBUGEND")
+    return {rank: [(i - min_ts) / 1000000 for i in events_time[rank]]
+            for rank in events_time}, events_type
 
 # Read the json file and keep events timestamp and type
 def read_json_data(file_name):
@@ -20,11 +66,7 @@ def read_json_data(file_name):
         events_time[entry["pid"]].append(entry["ts"])
         events_type[entry["pid"]].append(entry["name"])
     inf.close()
-    min_ts = min([min(events_time[rank]) for rank in events_time])
-    max_ts = max([max(events_time[rank]) for rank in events_time])
-    print("Execution time %d seconds" %((max_ts - min_ts) / 1000000))
-    return {rank: [(i - min_ts) / 1000000 for i in events_time[rank]]
-            for rank in events_time}, events_type
+    return add_edges(events_time, events_type)
 
 # update labels based on the new cluster split
 def create_labels(labels, split_index):
@@ -49,20 +91,22 @@ def find_clusters(events_time):
                     for i in range(1,len(events_time))]
     labels = [0] * len(events_time)
     best_clusters = [0, labels]
+    if len(labels) == 1:
+        return labels
     while len(events_delay) > 0:
         # Split clusters on the largerst delay between events
         max_delay = max([events_delay[i] for i in range(len(events_delay))
                          if labels[i]==labels[i+1]])
-        split_index = [i for i in range(len(events_delay))
-                       if events_delay[i]==max_delay][0]
-        print("Split between timestamp", events_time[split_index],
+        split_index = next(i for i in range(len(events_delay))
+                       if events_delay[i]==max_delay)
+        print("[dbg] Split between timestamp", events_time[split_index],
               events_time[split_index+1])
         # if the clusters start computing the silhouette, update the value for
-        # the current selection
-        if max_delay <= 60 and best_clusters[0]==0:
+        # the current selection (unless this is the first split)
+        if max_delay <= 60 and best_clusters[0] == 0 and max(labels) > 0:
             silhouette_avg = silhouette_score(events_time.reshape(-1, 1), labels)
             best_clusters = [silhouette_avg, labels[:]]
-            print("Store current cluster with a score of", silhouette_avg)
+            print("[dbg] Store current cluster with a score of", silhouette_avg)
         labels = create_labels(labels, split_index)
 
         # Compute the silhouette score which gives a score for the density and
@@ -71,15 +115,15 @@ def find_clusters(events_time):
         silhouette_avg = 0
         if max_delay <= 60:
             silhouette_avg = silhouette_score(events_time.reshape(-1, 1), labels)
-        print("Clustering score", silhouette_avg)
+        print("[dbg] Clustering score", silhouette_avg)
         if max_delay <= 60 and silhouette_avg > best_clusters[0]:
-            print("BEST cluster", best_clusters[0], silhouette_avg)
+            print("[dbg] BEST cluster", best_clusters[0], silhouette_avg)
             best_clusters = [silhouette_avg, labels[:]]
         if cannot_split_clusters(labels, max_delay):
             break
     if best_clusters[0] == 0:
             best_clusters = [silhouette_avg, labels[:]]
-    print("Total clusters", max(best_clusters[1]))
+    print("[dbg] Total clusters", max(best_clusters[1]) + 1)
     return best_clusters[1]
 
 # unorder and only count (not file information)
@@ -102,50 +146,247 @@ def get_count_pattern(cluster_edges, events_type, pattern_list):
         log.append(pattern_list[pattern_string])
     return pattern_list, log
 
-def add_file_information():
-    return 0
-
 # Extract all the event patterns describing patterns and their frequency
-def find_patterns(labels, events_type, events_time, pattern_list):
+def find_patterns(labels, events_type, events_time, pattern_list, rank):
     patterns = {}
     cluster_edges = [0] + [i+1 for i in range(len(labels) - 1)
                            if labels[i] != labels[i+1]] + [len(labels) + 1]
     time_clusters = {cluster_edges[i-1] : events_time[cluster_edges[i-1]:cluster_edges[i]]
                      for i in range(1, len(cluster_edges))}
-    print({i: (min(time_clusters[i]), max(time_clusters[i])) for i in time_clusters})
+    time_log = [(min(time_clusters[i]), max(time_clusters[i])) for i in time_clusters]
     pattern_list, log = get_count_pattern(cluster_edges, events_type, pattern_list)
-    print("Total patterns:", len(pattern_list.keys()), pattern_list.keys())
-    print("The current TAU log:",log)
-
-    # add information on what file is being accessed
-    add_file_information()
+    # Add timing information over the log: (pattern_id, time_start, time_end)
+    log = [(log[i], time_log[i][0], time_log[i][1], rank) for i in range(len(log))]
+    print("[dbg] Total patterns:", len(pattern_list.keys()), pattern_list)
+    print("[dbg] The current TAU log:",log)
     return pattern_list, log
 
 # Extract the start clusters based on patterns across all the ranks
-def extract_start_clusters(labels):
-    return 0
+# The log contains a list of (pattern_id, time_start, time_end)
+def extract_start_clusters(log, min_ts, max_ts):
+    start_idx = min_ts
+    for rank in log:
+        log_patterns = [i[0] for i in log[rank]]
+        # if not all patterns are occuring only once
+        if len(log_patterns) > len(set(log_patterns)):
+            # find the first pattern that occurs more than once
+            idx = next(i for i in range(1, len(log_patterns))
+                       if log_patterns.count(log_patterns[i]) > 1)
+            idx -= 1
+        else: # otherwise take all events that are closer to beginning
+            idx = next(i for i in range(len(log_patterns))
+                       if log[rank][i][0] - min_ts < max_ts - log[rank][i][1])
+        start_idx = max(start_idx, log[rank][idx][2])
+    return start_idx
 
 # Extract the end clusters based on patterns across all the ranks
-def extract_end_clusters(labels):
+# The log contains a list of (pattern_id, time_start, time_end)
+def extract_end_clusters(log, min_ts, max_ts):
+    end_idx = max_ts
+    for rank in log:
+        log_patterns = [i[0] for i in log[rank]]
+        # if not all patterns are occuring only once
+        if len(log_patterns) > len(set(log_patterns)):
+            # find the last pattern that occurs more than once
+            idx = next(i for i in reversed(range(1, len(log_patterns)))
+                       if log_patterns.count(log_patterns[i]) > 1)
+            idx += 1
+        else: # otherwise take all events that are closer to the end
+            idx = next(i for i in range(len(log_patterns))
+                       if log[rank][i][0] - min_ts > max_ts - log[rank][i][1])
+        end_idx = min(end_idx, log[rank][idx][2])
+    return end_idx
+
+def interpolate_comp_pattern(log, total_exec, req_exec, rank):
+    if total_exec > req_exec:
+        # cut all the events that exceed the requested execution time
+        return [i for i in log if i[2] < req_exec]
+    # if the requested time is larger than the existing log, extrapolate
+    pattern_ids = set([i[0] for i in log])
+    for pattern in pattern_ids:
+        ts_series = [i[1] for i in log if i[0]==pattern]
+        ts_series_end = [i[2] for i in log if i[0]==pattern]
+        try:
+            #The use of 1 signifies a linear fit.
+            fit = np.polyfit(range(len(ts_series)), ts_series, 2)
+            print("[dbg] Fit for cluster pattern", pattern, fit)
+            line = np.poly1d(fit)
+            cnt = len(ts_series)
+            while True:
+                ts = line(cnt)
+                if ts > req_exec:
+                    break
+                log += [(pattern, ts_series[cnt % len(ts_series)],
+                         ts_series_end[cnt % len(ts_series)], rank, ts)]
+                cnt += 1
+        except:
+            # we cannot use interpolation so we just repeat the pattern
+            print("[dbg] Repeat for cluster pattern", pattern, total_exec)
+            cnt = 1
+            while True:
+                # add entries total_exec apart
+                ts = [i + cnt * total_exec for i in ts_series]
+                log += [(pattern, ts_series[cnt % len(ts_series)],
+                         ts_series_end[cnt % len(ts_series)], rank, i)
+                        for i in ts if i <= req_exec]
+                if max(ts) > req_exec:
+                    break
+                cnt += 1
+    return log
+
+# Decrease or increase the total execution time of the log
+def update_exec_pattern(log, start_ts, end_ts, req_exec):
+    req_log = {rank:[] for rank in log}
+    total_exec = int(max([max([i[2] for i in log[rank]]) for rank in log]))
+    shift = req_exec - total_exec
+    print("[dbg] Shift by:", shift)
+    for rank in log:
+        # keep the start pattern
+        req_log[rank] += [i for i in log[rank] if i[2] <= start_ts]
+        # interpolate the middle pattern
+        print("[dbg] Generate extended log for rank", rank)
+        req_log[rank] += interpolate_comp_pattern(
+                [i for i in log[rank] if i[2] > start_ts and i[2] < end_ts],
+                end_ts, req_exec - (total_exec - end_ts), rank)
+        # shift the end pattern to the end of the request time
+        req_log[rank] += [(i[0], i[1], i[2], rank, i[1]+shift)
+                          for i in log[rank] if i[2] >= end_ts]
+    return req_log
+
+# extract clusters of ranks that have the exact same behavior
+def get_rank_exact_match(log):
+    rank_pattern = {}
+    for rank in log:
+        pattern = "".join([str(i[0]) for i in log[rank]])
+        if pattern not in rank_pattern:
+            rank_pattern[pattern] = []
+        rank_pattern[pattern].append(rank)
+    return rank_pattern
+
+# return clusters of ranks with the same behavior
+# Example output: [[0], [1, 2, 3, 4], [5, 6, 7, 8, 9]]
+def get_rank_pattern(log):
+    rank_pattern = get_rank_exact_match(log)
+    words = np.array(list(rank_pattern.keys())).reshape(-1, 1)
+    best_clusters = (0, list(range(len(words))))
+    for true_k in range(2, len(words)):
+        model = KMeans(n_clusters=true_k, init='k-means++',
+                       max_iter=100, n_init=1)
+        cluster = model.fit(words)
+        silhouette_avg = silhouette_score(words, cluster.labels_)
+        if silhouette_avg > best_clusters[0]:
+            best_clusters = (silhouette_avg, cluster.labels_)
+    # parse the clusters and create groups of rank ids
+    rank_clusters = []
+    words = [i[0] for i in words]
+    for cluster_id in range(max(best_clusters[1]) + 1):
+        temp = [rank_pattern[words[i]] for i in range(len(best_clusters[1]))
+                if best_clusters[1][i] == cluster_id]
+        rank_clusters.append([])
+        for i in temp:
+            rank_clusters[-1] += i
+    return rank_clusters
+
+# decreate or increase the number of ranks in the log
+def update_rank_pattern(log, total_ranks, req_ranks):
+    if req_ranks < total_ranks:
+        # delete the extra ranks from the log
+        for rank in range(req_ranks, total_ranks):
+            print("Delete", rank)
+            del log[rank]
+    else:
+        # find clusters of ranks with the same bahavior: [[0], [1, 2], [3]]
+        rank_cluster = get_rank_pattern(log)
+        # repeat the ranks that do not have individual behaviors
+        rank_pattern = [i for i in rank_cluster if len(i) > 1]
+        rank_pattern = list(roundrobin(rank_pattern))
+        if len(rank_pattern) == 0: # if all have individual patterns
+            rank_pattern = list(range(total_ranks)) # repeat all ranks
+        # add rank behavior in order based on the patterns found
+        cnt = 0
+        for rank in range(total_ranks, req_ranks):
+            print("[dbg] Rank %d will have the same pattern as rank %d" %(
+                rank, rank_pattern[rank % len(rank_pattern)]))
+            log[rank] = log[rank_pattern[rank % len(rank_pattern)]][:]
+    return log
+
+# Read the json file and keep a dictionary with strings
+# related to each timestamp
+def read_time_to_string(file_name):
+    inf = open(file_name, "r")
+    time_to_string = {}
+    json_data = json.load(inf)
+    min_ts = min([entry["ts"] for entry in json_data])
+    for entry in json_data:
+        if entry["pid"] not in time_to_string:
+            time_to_string[entry["pid"]] = {}
+        time_to_string[entry["pid"]][(entry["ts"]-min_ts)/1000000] = entry
+    inf.close()
+    return time_to_string
+
+# Create a JSON file from the log based on the input JSON
+# log is a dict {rank: [(pattern_id, ts_start, ts_end, initial_rank)]}
+def from_pattern_create_log(log, input_file, output_file):
+    max_exec = max(max([i[2] for i in log[rank]]) for rank in log)
+    print("Creating a TAU log file with %s ranks and %s execution time" %(
+        len(log.keys()), max_exec))
+    time_to_string = read_time_to_string(input_file)
+    data = []
+    for rank in log:
+        print("[dbg] Writing log for rank %d to the output file" %(rank))
+        for pattern in log[rank]:
+            # get all the entries occuring between the timestamps
+            create_events = [i for i in time_to_string[pattern[3]]
+                             if i <= pattern[2] and i >= pattern[1]]
+            delay = 0
+            if len(pattern)>4:
+                delay = pattern[4] - pattern[1]
+            event_list = [time_to_string[pattern[3]][i] for i in create_events]
+            for event in event_list:
+                temp_event = {i:event[i] for i in event}
+                temp_event["pid"] = rank
+                temp_event["ts"] = int(temp_event["ts"] + delay * 1000000)
+                data.append(temp_event)
+    with open(output_file, 'w') as f:
+        f.write(
+        '[' +
+        ',\n'.join(json.dumps(i) for i in data) +
+        ']\n')
+    print("DONE. Output json file stored in", output_file)
     return 0
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Usage: python %s log_file" %(sys.argv[0]))
+    if len(sys.argv) < 4:
+        print("Usage: python %s log_file num_ranks exec_time [output_file]" %(sys.argv[0]))
         exit()
     
+    req_ranks = int(sys.argv[2])
+    req_exec = int(sys.argv[3])
+    output_file = "output.json"
+    if len(sys.argv) > 4:
+        output_file = sys.argv[4]
     labels = {}
     # read the json TAU file
     events_time, events_type = read_json_data(sys.argv[1])
     pattern_list = {}
     log = {}
     for rank in events_time:
-        print("Extract clusters for rank", rank)
+        print("[dbg] Extract clusters for rank", rank)
         labels[rank] = find_clusters(np.array(events_time[rank]))
         pattern_list, log[rank] = find_patterns(labels[rank], events_type[rank],
-                                          events_time[rank], pattern_list)
-    extract_start_clusters(labels)
-    extract_end_clusters(labels)
+                                          events_time[rank], pattern_list, rank)
+    min_ts = min([min(events_time[rank]) for rank in events_time])
+    max_ts = max([max(events_time[rank]) for rank in events_time])
+    print("Number of ranks %d" %(len(events_time.keys())))
+    print("Execution time %d seconds" %(max_ts - min_ts))
+    start_ts = extract_start_clusters(log, min_ts, max_ts)
+    end_ts = extract_end_clusters(log, min_ts, max_ts)
+    print("[dbg] Start position", start_ts)
+    print("[dbg] End position", end_ts)
+    
+    if (max_ts - min_ts) != req_exec:
+        log = update_exec_pattern(log, start_ts, end_ts, req_exec)
+    if len(events_time.keys()) != req_ranks:
+        log = update_rank_pattern(log, len(events_time.keys()), req_ranks)
 
-    rank = 1
-    print(set(events_type[rank]))
+    from_pattern_create_log(log, sys.argv[1], output_file)
